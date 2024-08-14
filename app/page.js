79f94bc38acab3e883ebd9162a1ed9c0605bc46b
Sayed <sayed.sais@gmail.com>
2024-08-14 14:09:32 +0100

    added previous css and html

--- app/page.js
+++ app/page.js
@@ -1,95 +1,148 @@
 import Image from "next/image";
 import "./page.css";
 
-export default function Home() {
-  return (
-    <main className="main">
-      <div className="description">
-        <p>
-          Get started by editing&nbsp;
-          <code className="code">app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="vercelLogo"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="center">
-        <Image
-          className="logo"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="grid">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="card"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="card"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="card"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="card"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+function Header({title}){
+  return(
+    <header className="header-container">
+        <h1>{title}</h1>
+      </header> 
   );
 }
+
+function Hero1 ({imageU,sourceSet,size}) {
+  return(
+  <img
+  src={imageU}//"/images/hero-mobile.png"
+  srcSet={sourceSet}//"/images/hero-mobile.png 900w, /images/hero-desktop.png 901w"
+  sizes={size}//"(max-width: 900px) 100vw, 901px"
+  alt="Cozy living room with a fireplace"
+  className="hero-image"
+/>
+)}
+
+function Figure ({picture, alt, header, text}){
+  return(
+    <article className="tile">
+    <img
+    src={picture}
+    alt={alt}
+    className="tile-image"
+    />
+    <h3>{header}</h3>
+    <p>{text}
+    </p>
+    </article>
+  ) 
+}
+
+function Footer ({weblink1, text1,weblink2, text2,weblink3, text3 , email , emailtext}){
+
+  return(
+  <footer className="footer">
+          <div className="colunms-container">
+            <div className="footer-column">
+              <p>Find us on:</p>
+              <ul className="footer-nav">
+                <li>
+
+                  <a href={weblink1} className="socials-link">
+                  {text1}
+                    </a>
+                </li>
+                <li>
+                  <a href={weblink2} className="socials-link">
+                  {text2}
+                    </a>
+                </li>
+                <li>
+                  <a href={weblink3} className="socials-link">
+                  {text3}</a>
+                </li>
+              </ul>
+            </div>
+
+            <div className="footer-column">
+              <p>Contact us:</p>
+              <address>
+                <a href={email} className="email-link">
+                {emailtext}
+                  </a>
+              </address>
+            </div>
+          </div>
+        </footer>
+  )
+}
+
+export default function Home() {
+  return (
+  <body>
+      <div className="wrapper">
+      <Header 
+      title={"🔥Fireplace palace"}/>
+        <main>
+          <section className="hero-section">
+            <Hero1
+            src= {'app/images/hero-desktop.png'}
+            sourceSet={'app/images/hero-desktop.png 900w, app/images/hero-desktop.png 901w'}
+            imageURL={'(max-width: 900px) 100vw, 901px'}
+            />
+            <div className="hero-description">
+              <p className="hero-headline">
+                Discover the <br />
+                perfect fireplace ...
+              </p>
+              <p className="consultation-paragraph">
+                Book consultation: <span className="number-span">0121 345 6789</span>
+              </p>
+            </div>
+          </section>
+          <hr className="separator" />
+          <section id="how-it-works" className="how-it-works-section">
+            
+            <h2 className="how-it-works-text">How It Works</h2>
+            <div className="tiles">
+      
+                <Figure
+                  picture={"app/images/how-it-works-1.png"}
+                  alt={"Phone call illustration"}
+                  header={"Give us a call..."}
+                  text={"Call us and book in a 'Design Consultation' on a date and time that suits you."}
+                  article={""}
+                />
+               
+                <Figure
+                    picture={"/images/how-it-works-2.png"}
+                    alt={"Home visit illustration"}
+                    header={"We come to you..."}
+                    text={"We come to your home to do an assessment of the space and to your style preference."}
+                  />  
+            
+                <Figure
+                    picture={"/images/how-it-works-3.png"}
+                    alt={"Fireplace recommendation illustration"}
+                    header={"We recommend..."}
+                    text={"We send you a bespoke set of fireplace recommendations."}
+                    />
+            </div>
+          </section>
+        </main>
+        <Footer
+            weblink1={"https://www.facebook.com"}
+            text1={"Facebook"}
+           
+            weblink2={"https://www.instagram.com"}
+            text2={"Instgram"}
+          
+            weblink3={"https://www.tiktok.com"}
+            text3={"Tiktok"}
+
+            email={"mailto:info@firepalace.co.uk2"}
+            emailtext={"info@firepalace.co.uk"}
+            />
+      </div>
+    </body>
+  );
+}
+
+
