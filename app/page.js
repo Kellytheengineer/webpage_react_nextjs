f814a5a73442eff7654c0cd334f4408914aa45b8
Frankiegit config --global user.name Frankiegit config --global user.name Frankie <Frankiebrown996@gmail.com>
2024-08-15 15:33:18 +0100

    bugged link

--- app/page.js
+++ app/page.js
@@ -2,12 +2,12 @@ import Image from "next/image";
 import "./page.css";
 import HeroDescription from "../src/Hero/HeroDescription/HeroDescription.jsx";
 import HeroImage from "../src/Hero/HeroImage/HeroImage.jsx";
-import Link from 'next/link'
+
 
 export default function Home() {
   return (
     <main>
-      <Link href="/founders">Founders</Link>
+      
       <HeroImage/>
      <HeroDescription/>
 
