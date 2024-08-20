4421f14f0454e0c7cc264db1c7247703d576bfe6
Kellytheengineer <77806498+Kellytheengineer@users.noreply.github.com>
2024-08-20 11:02:56 +0100

    added form

--- app/booking/page.js
+++ app/booking/page.js
@@ -5,14 +5,39 @@ export default function Booking() {
       <main>
         <form>
         <fieldset>
+          <ul>
+             <li>
         <label for="fullName">Full Name</label>
             <input type="text" id="fullName" name="fullName"></input>
+            </li>
+            <li>
         <label for="postCode">Post Code</label>
             <input type="text" id="postCode" name="postCode"></input>           
+            </li>
+            <li>
         <label for="houseNumber">House/Flat Number and Street Name</label>
             <input type="text" id="houseNumber" name="houseNumber"></input>    
-            </fieldset>              
-    
+            </li>
+            <li>
+        <label for="city">City</label>
+            <input type="text" id="city" name="city"></input>    
+            </li>
+            </ul>
+            </fieldset> 
+            <fieldset> 
+            <h1>Contact Information:</h1>
+            <ul>
+            <li>
+        <label for="phoneNumber">Phone number</label>
+            <input type="text" id="phoneNumber" name="phoneNumber"></input>    
+            </li>  
+            <li>
+        <label for="email">Email Address</label>
+            <input type="email" id="email" name="email"></input>    
+            </li>  
+            </ul>
+            </fieldset>  
+            <button>Request Design Consultation</button>     
             </form>
        <h1>Booking</h1>
       </main>
