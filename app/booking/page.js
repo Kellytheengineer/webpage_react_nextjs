9f42012573d7b48331b8564686e3c4a452fbaf8b
Kellytheengineer <77806498+Kellytheengineer@users.noreply.github.com>
2024-08-22 12:23:01 +0100

    Form added diffrent states

--- app/booking/page.js
+++ app/booking/page.js
@@ -1,20 +1,13 @@
-"use client";
-
-import { useState, useReducer  } from 'react';
-
-
-//defining the states required for the reducer
+"use client";import { useState, useReducer  } from 'react';//defining the states required for the reducer
 const initialState = {
   data: {
       fullName: '',
     },
     status: false
-};
-
-//create a reducer function
+};//create a reducer function
 function reducer (state, action) {
   switch (action.type) {
-    case "CHANGE_FIELD":
+    case "CHANGE_FORM_DATA":
       return{
         ...state,
         data:{
@@ -27,36 +20,15 @@ function reducer (state, action) {
           ...state,
           status:"error"
         };
-        case "FORM_SUBMITTING":
-          return{
-            ...state,
-            status:"submitting"
-          };
-        case "FORM_SUCCESS":
-          return{
-            ...state,
-            status:"success"
-          };
     default:
       return state
   }
-}
-
-
-
-
-export default function Booking() {
-
-  //old states
+}export default function Booking() {  //old states
   const [ fullName, setFirstName ] = useState("");
-  const [ error, setError ] = useState(false);
-
-  // passing your initial state object 🙂
+  const [ error, setError ] = useState(false);  // passing your initial state object :slightly_smiling_face:
   const [ state, dispatch ] = useReducer(reducer, initialState);
-  console.log(state)
-
-  function handleChange(event){
-    if (event.target.name==="fullName"){ 
+  console.log(state)  
+  function handleChange (event){
         dispatch({
           type:"CHANGE_FORM_DATA",
           payload:{
@@ -64,48 +36,37 @@ export default function Booking() {
             value: event.target.value //defined by user
           }
         })
-    }}
-
-    function handleSubmit(event) {
+    }    function handleSubmit(event) {
       event.preventDefault();
       if (!state.data.fullName) //if name and value false return error, return dispatch object with type "ERROR"
      dispatch({
       type:"ERROR",
-     })
-
-
-  }
-
-    return (
+     })  }    return (
       <main>
         <form onSubmit={(event) => handleSubmit(event)}>
         <fieldset>
           <ul>
              <li>
                 <label for="fullName">Full Name</label>
-                <input 
-                  type="text" 
-                  id="fullName" 
-                  name="fullName" 
-                  value={state.data.fullName} //replace fullname
+                <input
+                  type="text"
+                  id="fullName"
+                  name="fullName"
+                  value={state.data.fullName}
                   onChange={(event)=>handleChange(event)}/>
             </li>
             </ul>
-            </fieldset> 
-            <fieldset> 
+            </fieldset>
+            <fieldset>
             <h1>Contact Information:</h1>
             <ul>
             </ul>
-            </fieldset>  
-            {state.status === "error" && <p> Value entered are not correct</p>} 
-            <button>Request Design Consultation</button>     
+            </fieldset>
+            {state.status === "error" && <p> Value entered are not correct</p>}
+            <button>Request Design Consultation</button>
             </form>
        <h1>Booking</h1>
       </main>
     );
-  }
-
-
-
-  // create a variable that holds the states that we previously created
+  }  // create a variable that holds the states that we previously created
   
