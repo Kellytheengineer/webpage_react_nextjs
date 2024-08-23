a5bdd1f143b81b36d896446c323d29659c62a688
Sayed <sayed.sais@gmail.com>
2024-08-23 09:41:58 +0100

    added fields to form

--- app/booking/page.js
+++ app/booking/page.js
@@ -1,110 +1,154 @@
 "use client";
 
-import { useReducer  } from 'react';
+import { useReducer } from "react";
 import "./page.css";
 
 //defining the states required for the reducer
 const initialState = {
   data: {
-      fullName: '',
-    },
-    status: 'editing'
+    fullName: "",
+    postcode: "",
+    houseNumber: "",
+    city: ""
+  },
+  status: "editing",
 };
 
 //create a reducer function
-function reducer (state, action) {
+function reducer(state, action) {
   switch (action.type) {
     case "CHANGE_FORM_DATA":
-      return{
+      return {
         ...state,
-        data:{
+        data: {
           ...state.data,
-          [action.payload.name]:action.payload.value
-        }
+          [action.payload.name]: action.payload.value,
+        },
       };
-      case 'ERROR':
-        return{
-          ...state,
-          status:"error"
+    case "ERROR":
+      return {
+        ...state,
+        status: "error",
       };
-      case 'SUBMITTING':
-        return{
-          ...state,
-          status:"submitting"
-        };
-      case "FORM_SUCCESS":
-        return{
-          ...state,
-          status:"success"
-        };
+    case "SUBMITTING":
+      return {
+        ...state,
+        status: "submitting",
+      };
+    case "FORM_SUCCESS":
+      return {
+        ...state,
+        status: "success",
+      };
     default:
-      return state
+      return state;
+  }
+}
+export default function Booking() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  console.log(state);
+
+  function handleChange(event) {
+    dispatch({
+      type: "CHANGE_FORM_DATA",
+      payload: {
+        name: event.target.name, //fullName
+        value: event.target.value, //defined by user
+      },
+    });
   }
 
-}export default function Booking() { 
-  const [ state, dispatch ] = useReducer(reducer, initialState);
-
-  console.log(state)  
-
-  function handleChange (event){
-    dispatch({
-      type:"CHANGE_FORM_DATA",
-      payload:{
-        name: event.target.name, //fullName
-        value: event.target.value //defined by user
-      }
-      })
-    }    
-    
-    function handleSubmit(event) {
+  function handleSubmit(event) {
     event.preventDefault();
-    
-    dispatch({
-      type:"SUBMITTING",
-    })  
-    
-    setTimeout(() => {
-    if (!state.data.fullName) {
-     dispatch({
-      type:"ERROR",
-     })  
-    }
-    dispatch({
-      type:"FORM_SUCCESS",
-    })  
 
+    dispatch({
+      type: "SUBMITTING",
+    });
+
+    setTimeout(() => {
+      if (!state.data.fullName || !state.data.postcode || !state.data.houseNumber || !state.data.city ) {
+        // add post code and house number to if statement
+        dispatch({
+          type: "ERROR",
+        });
+      } else
+        dispatch({
+          type: "FORM_SUCCESS",
+        });
     }, 3000);
-  }    
-     
-  
+  }
+
   return (
-      <main>
-        <form onSubmit={(event) => handleSubmit(event)}>
-        <fieldset>
-        {state.status === "success" && <p className='succesPage'> Thank you for the submission!</p>}
+    <main>
+      <form onSubmit={(event) => handleSubmit(event)} className="form">
+        <fieldset className="group">
+          {state.status === "success" && (
+            <p className="succesPage"> Thank you for the submission!</p>
+          )}
+          <legend className="groupTitle">Personal Info:</legend>
           <ul>
-             <li>
-                <label for="fullName">Full Name</label>
-                <input
-                  type="text"
-                  id="fullName"
-                  name="fullName"
-                  value={state.data.fullName}
-                  onChange={(event)=>handleChange(event)}/>
-            </li>
-            </ul>
-            </fieldset>
-            <fieldset>
-            <h1>Contact Information:</h1>
-            <ul>
-            </ul>
-            </fieldset>
-            {state.status === "error" && <p> Value entered are not correct</p>}
-            {state.status}
-            {state.status === "submitting" || <button>Request Design Consultation</button>}
-            </form>
-       <h1>Booking</h1>
-      </main>
-    );
-  }  
-  
+            <label className="inputGroup" for="fullName">
+              Full Name
+            </label>
+            <input
+              type="text"
+              id="fullName"
+              name="fullName"
+              value={state.data.fullName}
+              onChange={(event) => handleChange(event)}
+              className="input"
+            />
+            {/* error message for fullName */}
+            {state.status === "error" && <p className="error"> Full name error</p>}
+            <label className="inputGroup" for="postcode">
+              Post Code
+            </label>
+            <input
+              type="text"
+              id="postcode"
+              name="postcode"
+              value={state.data.postcode}
+              onChange={(event) => handleChange(event)}
+              className="input"
+            />
+            <label className="inputGroup" for="houseNumber">
+              House/Flat Number and Street Name
+            </label>
+            <input
+              type="text"
+              id="houseNumber"
+              name="houseNumber"
+              value={state.data.houseNumber}
+              onChange={(event) => handleChange(event)}
+              className="input"
+            />
+            <label className="inputGroup" for="city">
+              City
+            </label>
+            <input
+              type="text"
+              id="city"
+              name="city"
+              value={state.data.city}
+              onChange={(event) => handleChange(event)}
+              className="input"
+            />
+          </ul>
+        </fieldset>
+        <fieldset className="group">
+          <legend className="groupTitle">Contact Info:</legend>
+          <ul></ul>
+        </fieldset>
+        {state.status === "error" && (
+          <p className="error">Value entered are not correct</p>
+        )}
+        {state.status}
+        {state.status === "submitting" || (
+          <button className="button">Request Design Consultation</button>
+        )}
+      </form>
+      <h1>Booking</h1>
+    </main>
+  );
+}
--- plan.md
+++ plan.md
@@ -9,4 +9,24 @@ put in the useReducer✅
 replace the fullName with the appropriate reducer names✅
 Update handle change event with dispatch✅
 
-update the reducer function to interact with the handle change function✅
+update the reducer function to interact with the handle change functionc
+
+Implement different error state message upon form submission✅
+When for is submitting create a dispatcher which will have type form submitting✅
+Add a switch statement which will update state ✅
+
+As a user when I submit the form it should show me that it is submitting.
+use the 'success' mesage in state
+
+As a user I should not be able to submit again while it is submitting.
+As a user when I submit the form in a correct way (passes validation) I should see the form replaced with a simple success message.
+As a user when I submit the form in an incorrect way I should see that the form is no longer submitting and that there are errors.
+
+Hiding form on success
+
+when status = success then show message(div/P)
+
+To do:
+
+- add fields for contact information
+  -As a user who has incorrectly submitted the form I should see an error under each field that has failed validation. The error should be specific to the field ("Not a valid phone number", "No bookings outside of England, Wales or Scotland").
